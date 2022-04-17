207ae9f23e56c9c07c9f92ea3f9166239c9d077c
ZMXC01 <78628186+ZMXC01@users.noreply.github.com>
2022-04-17 18:02:35 +0800

    修复每个学期大学习结束后，持续运行bug

--- nonebot_plugin_youthstudy/__init__.py
+++ nonebot_plugin_youthstudy/__init__.py
@@ -7,6 +7,7 @@ from .getdata import get_answer
 from nonebot.log import logger
 from datetime import datetime
 from .config import Config
+from .getScr import get_Src
 
 global_config = nonebot.get_driver().config
 nonebot.logger.info("global_config:{}".format(global_config))
@@ -25,14 +26,17 @@ from nonebot import require
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 # 每周一10:00开始检测是否更新，每3分检测一次，觉得检测间隔太久，请手动修改time.sleep()，获取到答案后终止检测。
-@scheduler.scheduled_job('cron', day_of_week='0', hour=10, minute=0)
+@scheduler.scheduled_job('cron', day_of_week='0-6', hour=17, minute=20)
 async def remind():
         try:
+            num = 0
             for i in range(10000):
                 img = await get_answer()
                 if img is None or img == '未找到答案':
                     time.sleep(180)
+                    num += 1
                 else:
+                    url = await get_Src()
                     message = [
                         {
                             "type": "text",
@@ -44,6 +48,18 @@ async def remind():
                             "type": "image",
                             "data": {
                                 "file": img
+                            }
+                        },
+                        {
+                            "type": "text",
+                            "data": {
+                                "text": '完成截图'
+                            }
+                        },
+                        {
+                            "type": "image",
+                            "data": {
+                                "file": url
                             }
                         }
                     ]
@@ -57,6 +73,24 @@ async def remind():
                                                                message="[CQ:at,qq={}]{}".format("all", message))
                         time.sleep(1)
                     break
+                if num >= 200:
+                    message = [
+                        {
+                            "type": "text",
+                            "data": {
+                                "text": '本周没有大学习哦！'
+                            }
+                        }
+                    ]
+                    # 给配置的列表里的qq好友发通知
+                    for qq in plugin_config.qq_friends:
+                        await nonebot.get_bot().send_private_msg(user_id=qq, message=message)
+                        time.sleep(1)
+                        # 给群发送通知
+                    for qq_group in plugin_config.qq_groups:
+                        await nonebot.get_bot().send_group_msg(group_id=qq_group,
+                                                               message="[CQ:at,qq={}]{}".format("all", message))
+                        time.sleep(1)
         except Exception as e:
             with open('./log.txt', 'a+', encoding='utf-8') as f:
                 f.write(str(e))
