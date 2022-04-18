654e6bb715375b7fdf3ed33ab9e688e381bee028
ZMXC01 <78628186+ZMXC01@users.noreply.github.com>
2022-04-18 17:35:41 +0800

    添加自动获取完成截图功能

--- nonebot_plugin_youthstudy/__init__.py
+++ nonebot_plugin_youthstudy/__init__.py
@@ -7,7 +7,7 @@ from .getdata import get_answer
 from nonebot.log import logger
 from datetime import datetime
 from .config import Config
-from .getScr import get_Src
+from .get_src import get_pic
 
 global_config = nonebot.get_driver().config
 nonebot.logger.info("global_config:{}".format(global_config))
@@ -26,22 +26,32 @@ from nonebot import require
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 # 每周一10:00开始检测是否更新，每3分检测一次，觉得检测间隔太久，请手动修改time.sleep()，获取到答案后终止检测。
-@scheduler.scheduled_job('cron', day_of_week='0-6', hour=17, minute=20)
+@scheduler.scheduled_job('cron', day_of_week='0', hour=10, minute=0)
 async def remind():
         try:
             num = 0
-            for i in range(10000):
+            for i in range(960):
                 img = await get_answer()
                 if img is None or img == '未找到答案':
                     time.sleep(180)
                     num += 1
                 else:
-                    url = await get_Src()
+                    content = await get_pic()
+                    title = content[0]
+                    starttime = content[1]
+                    cover = content[2]
+                    end_url = content[3]
                     message = [
                         {
                             "type": "text",
                             "data": {
-                                "text": '本周的青年大学习开始喽！'
+                                "text": '本周的青年大学习开始喽！\n'+ title +'\n开始时间：'+ starttime +'\n答案见图二、完成截图见图三\nPs:如果学校会查后台记录，\n请前往相应平台观看1分钟，\n确保在后台留下观看记录！！！'
+                            }
+                        },
+                        {
+                            "type": "image",
+                            "data": {
+                                "file": cover
                             }
                         },
                         {
@@ -50,16 +60,11 @@ async def remind():
                                 "file": img
                             }
                         },
-                        {
-                            "type": "text",
-                            "data": {
-                                "text": '完成截图'
-                            }
-                        },
+
                         {
                             "type": "image",
                             "data": {
-                                "file": url
+                                "file": end_url
                             }
                         }
                     ]
@@ -74,7 +79,7 @@ async def remind():
                         time.sleep(1)
                     break
                 if num >= 200:
-                    message = [
+                    message1 = [
                         {
                             "type": "text",
                             "data": {
@@ -84,13 +89,14 @@ async def remind():
                     ]
                     # 给配置的列表里的qq好友发通知
                     for qq in plugin_config.qq_friends:
-                        await nonebot.get_bot().send_private_msg(user_id=qq, message=message)
+                        await nonebot.get_bot().send_private_msg(user_id=qq, message=message1)
                         time.sleep(1)
                         # 给群发送通知
                     for qq_group in plugin_config.qq_groups:
                         await nonebot.get_bot().send_group_msg(group_id=qq_group,
-                                                               message="[CQ:at,qq={}]{}".format("all", message))
+                                                               message="[CQ:at,qq={}]{}".format("all", message1))
                         time.sleep(1)
+                    break
         except Exception as e:
             with open('./log.txt', 'a+', encoding='utf-8') as f:
                 f.write(str(e))
