a94a609a8681ee780c36e57c9af9b0200555efc3
ayanamiblhx <2927652162@qq.com>
2022-09-08 10:24:01 +0800

    :bug: 修复无法获取截图的bug

--- nonebot_plugin_youthstudy/get_src.py
+++ nonebot_plugin_youthstudy/get_src.py
@@ -12,13 +12,12 @@ async def get_pic():
     try:
         async with AsyncClient(headers=headers, proxies={"all://": None}) as client:
             response = await client.get(req_url)
-            response.encoding = response.apparent_encoding
             json_obj = json.loads(response.text)
             cover = json_obj['result']['cover']
-            try:
-                end = json_obj['result']['uri'].replace('index.html', 'images/end.jpg')
-            except Exception as e:
-                logger.error(e)
+            end = json_obj['result']['uri']
+            if end.find("index.html") != -1:
+                end = end.replace('index.html', 'images/end.jpg')
+            elif end.find("m.html") != -1:
                 end = json_obj['result']['uri'][:-6] + 'images/end.jpg'
             start_time = json_obj['result']['startTime']
             title = json_obj['result']['title']
